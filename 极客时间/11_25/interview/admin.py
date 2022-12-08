5b04c79072ba90ae2ae4bc4e6d0a6802b3d926e0
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-08 17:11:22 +0800

    Update admin.py

--- 极客时间/11_25/interview/admin.py
+++ 极客时间/11_25/interview/admin.py
@@ -1,10 +1,43 @@
+import csv
+from datetime import datetime
 from django.contrib import admin
-
-# Register your models here.
+from django.http import HttpResponse
 from interview.models import Candidate
+
+
+exportable_fields = ('username', 'city', 'phone', 'bachelor_school', 'master_school', 'degree', 'first_result', 'first_interviewer_user',
+                     'second_result', 'second_interviewer_user', 'hr_result', 'hr_score', 'hr_remark', 'hr_interviewer_user')
+
+def export_model_as_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    field_list = exportable_fields
+    response['Content-Disposition'] = 'attachment; filename=%s-list-%s.csv' % (
+        'candidates',
+        datetime.now().strftime('%Y-%m-%d-%H-%M-%S'),
+    )
+
+    # 写入表头
+    writer = csv.writer(response)
+    writer.writerow(
+        [queryset.model._meta.get_field(f).verbose_name.title() for f in field_list],
+    )
+
+    for obj in queryset:
+        ## 单行 的记录（各个字段的值）， 根据字段对象，从当前实例 (obj) 中获取字段值
+        csv_line_values = []
+        for field in field_list:
+            field_object = queryset.model._meta.get_field(field)
+            field_value = field_object.value_from_object(obj)
+            csv_line_values.append(field_value)
+        writer.writerow(csv_line_values)
+    return response
+
+export_model_as_csv.short_description = u'导出为CSV文件'
+
 
 # 候选人管理类
 class CandidateAdmin(admin.ModelAdmin):
+    actions = [export_model_as_csv,]
     exclude = ('creator', 'created_date', 'modified_date')
     list_display = (
         'username', 'city', 'bachelor_school','first_score', 'first_result',
