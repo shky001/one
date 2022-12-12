8940c714333cddc538ef8f00753f0a4d82e575f2
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-12 16:33:12 +0800

    Create local.py

---
+++ 极客时间/11_25/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+LDAP_AUTH_URL = "ldap://xxxxx:389"
+LDAP_AUTH_CONNECTION_USERNAME = "admin"
+
+INSTALLED_APPS += (
+    # other apps for production site
+)
+
+## 钉钉群的 WEB_HOOK， 用于发送钉钉消息
+DINGTALK_WEB_HOOK = "https://oapi.dingtalk.com/robot/send?access_token=e9101fb417f6afce2a7d96e81ff560e33908611323cb82e66f01cd433cc4cbb3"
