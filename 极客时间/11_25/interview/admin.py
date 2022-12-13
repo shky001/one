a07767768fad1900eb737bb22889793da9083cf9
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-13 19:17:34 +0800

    Update admin.py

--- 极客时间/11_25/interview/admin.py
+++ 极客时间/11_25/interview/admin.py
@@ -1,15 +1,19 @@
 import csv
 from datetime import datetime
 from django.contrib import admin
+from django.db.models import Q
 from django.http import HttpResponse
 from interview.models import Candidate
+from interview import candidate_fieldset as cf
+from interview import dingtalk
 
 
 exportable_fields = ('username', 'city', 'phone', 'bachelor_school', 'master_school', 'degree', 'first_result', 'first_interviewer_user',
                      'second_result', 'second_interviewer_user', 'hr_result', 'hr_score', 'hr_remark', 'hr_interviewer_user')
 
+# 导出至csv文件
 def export_model_as_csv(modeladmin, request, queryset):
-    response = HttpResponse(content_type='text/csv')
+    response = HttpResponse(content_type='text/csv', charset='utf-8-sig')
     field_list = exportable_fields
     response['Content-Disposition'] = 'attachment; filename=%s-list-%s.csv' % (
         'candidates',
@@ -33,16 +37,31 @@ def export_model_as_csv(modeladmin, request, queryset):
     return response
 
 export_model_as_csv.short_description = u'导出为CSV文件'
+export_model_as_csv.allowed_permissions = ('export',)
+
+
+# 通知一面面试官面试
+def notify_interviewer(modeladmin, request, queryset):
+    candidates = ""
+    interviewers = ""
+    for obj in queryset:
+        candidates = obj.username + ";" + candidates
+        interviewers = obj.first_interviewer_user.username + ";" + interviewers
+    # 消息发送到钉钉
+    dingtalk.send ("通知：候选人 %s 进入面试环节，亲爱的面试官，请准备好面试： %s" % (candidates, interviewers) )
+
+notify_interviewer.short_description = u'通知一面面试官'
 
 
 # 候选人管理类
 class CandidateAdmin(admin.ModelAdmin):
-    actions = [export_model_as_csv,]
+    actions = [export_model_as_csv, notify_interviewer]
     exclude = ('creator', 'created_date', 'modified_date')
-    list_display = (
-        'username', 'city', 'bachelor_school','first_score', 'first_result',
-        'second_result', 'hr_score', 'hr_result', 'last_editor')
 
+    list_display = (
+        'username', 'city', 'bachelor_school', 'first_score', 'first_result', 'first_interviewer_user',
+        'second_score',
+        'second_result', 'second_interviewer_user', 'hr_score', 'hr_result', 'hr_interviewer_user',)
     # 右侧筛选条件
     list_filter = (
     'city', 'first_result', 'second_result', 'hr_result', 'first_interviewer_user', 'second_interviewer_user',
@@ -55,21 +74,65 @@ class CandidateAdmin(admin.ModelAdmin):
     ordering = ('hr_result', 'second_result', 'first_result',)
 
     # 分组展示字段，分三块，基础信息、第一轮面试记录、第二轮面试（专业复试）、HR复试
-    fieldsets = (
-        (None, {'fields': ("userid", ("username", "city", "phone"),
-                           ("email", "apply_position", "born_address", "gender", "candidate_remark"),
-                           ("bachelor_school", "master_school", "doctor_school"), ("major", "degree"),
-                           ("test_score_of_general_ability", "paper_score"),)}),
-        ('第一轮面试', {'fields': (
-        ("first_score", "first_learning_ability", "first_professional_competency"), "first_advantage",
-        "first_disadvantage", "first_result", "first_recommend_position", "first_remark",)}),
-        ('第二轮面试（专业复试）', {'fields': (("second_score", "second_learning_ability", "second_professional_competency"), (
-        "second_pursue_of_excellence", "second_communication_ability", "second_pressure_score"), "second_advantage",
-                                    "second_disadvantage", "second_result", "second_recommend_position",
-                                    "second_remark",)}),
-        ('HR复试', {'fields': ("hr_score", ("hr_responsibility", "hr_communication_ability", "hr_logic_ability"),
-                             ("hr_potential", "hr_stability"), "hr_advantage", "hr_disadvantage", "hr_result",
-                             "hr_remark",)}),
-    )
+
+    def get_readonly_fields(self, request, obj):
+        group_names = self.get_group_names(request.user)
+
+        if 'interviewer' in group_names:
+            # logger.info("interviewer is in user's group for %s" % request.user.username)
+            return ('first_interviewer_user', 'second_interviewer_user',)
+        return ()
+
+    def get_group_names(self, user):
+        group_names = []
+        for g in user.groups.all():
+            group_names.append(g.name)
+        return group_names
+
+    list_editable = ('first_interviewer_user','second_interviewer_user',)
+    def get_list_editable(self, request):
+        group_names = self.get_group_names(request.user)
+
+        if request.user.is_superuser or 'hr' in group_names:
+            return ('first_interviewer_user', 'second_interviewer_user',)
+        return ()
+
+    def get_changelist_instance(self, request):
+        """
+        override admin method and list_editable property value
+        with values returned by our custom method implementation.
+        """
+        self.list_editable = self.get_list_editable(request)
+        return super(CandidateAdmin, self).get_changelist_instance(request)
+
+    # 一面面试官仅填写一面反馈， 二面面试官可以填写二面反馈
+    def get_fieldsets(self, request, obj=None):
+        group_names = self.get_group_names(request.user)
+        # 一面二面相同
+        if 'interviewer' in group_names and obj.first_interviewer_user == request.user and obj.second_interviewer_user == request.user:
+            return cf.default_fieldsets_second
+
+        if 'interviewer' in group_names and obj.first_interviewer_user == request.user:
+            return cf.default_fieldsets_first
+        if 'interviewer' in group_names and obj.second_interviewer_user == request.user:
+            return cf.default_fieldsets_second
+
+        return cf.default_fieldsets
+
+
+    # 对于非管理员，非HR，获取自己是一面面试官或者二面面试官的候选人集合:s
+    def get_queryset(self, request):  # show data only owned by the user
+        qs = super(CandidateAdmin, self).get_queryset(request)
+
+        group_names = self.get_group_names(request.user)
+        if request.user.is_superuser or 'hr' in group_names:
+            return qs
+        return Candidate.objects.filter(
+            Q(first_interviewer_user=request.user) | Q(second_interviewer_user=request.user))
+
+    # 当前用户是否有导出权限：
+    def has_export_permission(self, request):
+        opts = self.opts
+        return request.user.has_perm('%s.%s' % (opts.app_label, "export"))
 
 admin.site.register(Candidate,CandidateAdmin)
