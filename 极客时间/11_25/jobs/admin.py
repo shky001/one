f36bf9de818c111e31ebd1201803b61929be631d
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-15 20:02:24 +0800

    Update admin.py

--- 极客时间/11_25/jobs/admin.py
+++ 极客时间/11_25/jobs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from jobs.models import Job
+from jobs.models import Resume
+
 # Register your models here.
 
 # 定义管理类
@@ -16,5 +18,26 @@ class JobAdmin(admin.ModelAdmin):
         # 调用父类方法保存对象
         super().save_model(request, obj, form, change)
 
+
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('username', 'applicant', 'city', 'apply_position', 'bachelor_school', 'master_school', 'major','created_date')
+
+    readonly_fields = ('applicant', 'created_date', 'modified_date',)
+
+    fieldsets = (
+        (None, {'fields': (
+            "applicant", ("username", "city", "phone"),
+            ("email", "apply_position", "born_address", "gender", ), ("picture", "attachment",),
+            ("bachelor_school", "master_school"), ("major", "degree"), ('created_date', 'modified_date'),
+            "candidate_introduction", "work_experience","project_experience",)}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.applicant = request.user
+        super().save_model(request, obj, form, change)
+
+
+
 # admin.site.register(Job)
 admin.site.register(Job,JobAdmin)
+admin.site.register(Resume, ResumeAdmin)
