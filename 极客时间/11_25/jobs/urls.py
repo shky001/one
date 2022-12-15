475a8344bd2c3c284f33275be3b5ef8956d0bbe1
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-15 20:03:16 +0800

    Update urls.py

--- 极客时间/11_25/jobs/urls.py
+++ 极客时间/11_25/jobs/urls.py
@@ -1,8 +1,19 @@
 # from django.conf.urls import url
+from django.urls import path
+
 from django.urls import re_path as url
 
 from jobs import views
 
 urlpatterns = [
-    url(r"^joblist/",views.joblist,name = "joblist")
+    url(r"^joblist/",views.joblist,name = "joblist"),
+    url(r"^job/(?P<job_id>\d+)/$",views.detail,name="detail"),
+
+    # 首页自动跳转到 职位列表
+    url(r"^$", views.joblist, name="name"),
+    # path("", views.joblist, name="name"),
+
+    # 提交简历
+    path('resume/add/', views.ResumeCreateView.as_view(), name='resume-add'),
+
 ]
