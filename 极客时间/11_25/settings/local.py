41911dcaaa12b833c5d3475ee030d04b7b5c0f47
shky001 <107673545+shky001@users.noreply.github.com>
2022-12-21 18:24:06 +0800

    Update local.py

--- 极客时间/11_25/settings/local.py
+++ 极客时间/11_25/settings/local.py
@@ -11,5 +11,15 @@ INSTALLED_APPS += (
     # other apps for production site
 )
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 ## 钉钉群的 WEB_HOOK， 用于发送钉钉消息
 DINGTALK_WEB_HOOK = "https://oapi.dingtalk.com/robot/send?access_token=e9101fb417f6afce2a7d96e81ff560e33908611323cb82e66f01cd433cc4cbb3"
