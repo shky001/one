a748d6f4cd8222e65f6a299c01c6323315f3378f
shky001 <107673545+shky001@users.noreply.github.com>
2023-02-08 17:13:47 +0800

    Create urls.py

--- 极客时间/11_25/Test_Django01/urls.py
+++ 极客时间/11_25/Test_Django01/urls.py
@@ -74,6 +74,16 @@ urlpatterns = [
 ]
 
 
+from django.conf.urls import include
+from django.conf import settings
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+
 from django.conf import settings
 from django.conf.urls.static import static
 if settings.DEBUG:
