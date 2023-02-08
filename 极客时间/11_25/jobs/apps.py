a3a9bef4a47cad5fec26e5f315a3d908a08efffd
shky001 <107673545+shky001@users.noreply.github.com>
2023-02-08 17:14:19 +0800

    Update apps.py

--- 极客时间/11_25/jobs/apps.py
+++ 极客时间/11_25/jobs/apps.py
@@ -1,6 +1,20 @@
+# from django.apps import AppConfig
+#
+#
+# class JobsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'jobs'
+
+
+
 from django.apps import AppConfig
+import logging
+logger = logging.getLogger(__name__)
 
 
-class JobsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class JobConfig(AppConfig):
     name = 'jobs'
+
+    def ready(self):
+        logger.info("JobConfig ready")
+        from jobs.signal_processor import post_save_callback
